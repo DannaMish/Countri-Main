25fc7406a68dbca2ca4b1dc2d6dbb443991a869f
DannaMish <zachary_sex30@hotmail.com>
2022-12-08 22:45:57 -0500

    Database creation progress

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -8,5 +8,22 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    continent:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flag:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    capital:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   });
 };
